2a400cdd268a4c46a5a7b1e3add3f722843ee6a2
Prakash Bhattarai <lightvelocity888@gmail.com>
2022-06-30 17:20:07 +0545

    Update welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-    return <div>Welcome to Movies</div>
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>Web based app that suggests movies for WRC students.</p>
+        </div>
+    )
 }
 
 export default App
